ea2c02e481f4af35d9ba4cc0b5ffa3259e3240c7
Erez Shtayman <erezstmn@gmail.com>
2018-02-21 13:38:44 +0200

    first connection between client server and db

--- server/index.js
+++ server/index.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const path = require('path');
-
+const mysql = require('mysql');
 const app = express();
 const PORT = process.env.PORT || 5000
 
 
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+const mySqlHost = "localhost";
+
+
+
 app.get('/', (req, res) => {
     res.set('Content-Type', 'application/json');
     res.send('Hello from node server');
 });
 app.get('/movies/', (req, res) => {
     console.log('get request received')
-    res.send(`Hello from the servre running on ${PORT}`);
+    const connection = mysql.createConnection({
+        host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user:'vddz3a51wdk49rc4',
+        password:'pxh153k7dxyuvt4n'
+    });
+    connection.connect((error) => {
+        if (error){
+            return console.log(error);
+        }    
+        console.log('Connected!');
+    })
+    connection.query("SELECT * FROM x00qcbxug94h1hki.movies", (error, result) => {
+        if (error){
+            return console.log(error);
+        }  
+        res.send(result);
+    });  
+    connection.end();  
 })
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
