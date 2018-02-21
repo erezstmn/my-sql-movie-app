d5a9dbf92fc3c81e71116bfc3e6b6bc7de5c15e1
Erez Shtayman <erezstmn@gmail.com>
2018-02-21 16:21:26 +0200

    score movie

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -8,27 +8,311 @@ import path from 'path';
 class App extends Component {
   constructor(){
     super();
-    this.handleButton = this.handleButton.bind(this);
-    
-  }
-  handleButton(){
-    axios.get('../movies').then((res) => {
-      console.log(res);
-    }).catch((e) => {
-      console.log(e);
-    })
+    this.state = {
+      hideNewMovieForm : true,
+      hideRankMovieForm : true,
+      hideSearchMovieForm : true,
+      hideAddNewMovieButton : false,
+      hideRankMovieButton : false,
+      hideSearchMovieButton : false,
+      hideErrorMessage : true,
+      list: []
+  
+    };
+    this.handleAddNewMovie = this.handleAddNewMovie.bind(this);
+    this.handleRateMovie = this.handleRateMovie.bind(this);
+    this.handleSearchMovie = this.handleSearchMovie.bind(this);    
+    this.handleSubmitNewMovie = this.handleSubmitNewMovie.bind(this);
+    this.handleSubmitScore = this.handleSubmitScore.bind(this);
+    this.handleSearchMovieSubmit = this.handleSearchMovieSubmit.bind(this);
   };  
+  handleAddNewMovie(){
+    this.setState(() =>{
+      return ({
+        hideNewMovieForm : false,
+        hideRankMovieForm : true,
+        hideSearchMovieForm : true,
+        hideAddNewMovieButton : true,
+        hideRankMovieButton : false,
+        hideSearchMovieButton : false,
+        hideErrorMessage : true,
+        list:[]
+      });
+    });    
+  }
+  handleSubmitNewMovie(e) {
+    e.preventDefault();
+    const name = document.getElementById("newMovieName").value.toLowerCase().trim();    
+    const genre = document.getElementById("newMovieGenre").value.toLowerCase();
+    const year = document.getElementById("newMovieYear").value;
+    if ((name.trim().length === 0) || (!genre) || (!year)){
+      this.setState(() => {
+        return ({
+          hideErrorMessage :false
+        });
+      });
+      return console.log("error");
+    }
+    this.setState(() => {
+      return ({
+        hideErrorMessage : true
+      });
+    });     
+    axios.get(`../movies?movie_name=${name}`)
+    .then((response) => {
+      console.log(response.data);
+      if(response.data.length !==0 ){
+        alert(`The movie "${name}" already exists in the db`);
+      } else{
+        axios.post('../movies', {
+      movie_name: name,
+      genre: genre,
+      release_year: year
+    })
+    .then((response) => {
+      console.log(response);
+      alert(`The movie "${name}" was added to the DB`);
+      this.setState(() =>{
+        return ({
+          hideNewMovieForm : true,
+          hideRankMovieForm : true,
+          hideSearchMovieForm : true,
+          hideAddNewMovieButton : false,
+          hideRankMovieButton : false,
+          hideSearchMovieButton : false,
+          hideErrorMessage : true,
+          list:[]
+        });
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+      }     
+    })
+    .catch((error) => {
+      console.log(error);
+    });    
+  };
+  handleRateMovie(){
+    this.setState(() =>{
+      return ({
+        hideNewMovieForm : true,
+        hideRankMovieForm : false,
+        hideSearchMovieForm : true,
+        hideAddNewMovieButton : false,
+        hideRankMovieButton : true,
+        hideSearchMovieButton : false,
+        hideErrorMessage : true,
+        list:[]
+      });
+    });   
+  };
+  handleSubmitScore(e) {
+    e.preventDefault();
+    const name = document.getElementById("rankMovieName").value.toLowerCase().trim();
+    const score = document.getElementById("score").value;
+    if ((name.trim().length === 0) || (!score)){
+      this.setState(() => {
+        return ({
+          hideErrorMessage :false
+        });
+      });
+      return console.log("error");
+    }
+    this.setState(() => {
+      return ({
+        hideErrorMessage : true
+      });
+    });
+    axios.get(`../movies?movie_name=${name}`)
+    .then((response) => {
+      if(response.data.length === 0) {
+        alert(`The movie ${name} does not exist in the db`);
+      } else{
+       axios.patch(`../movies?movie_name=${name}&score=${score}`)
+       .then((response) => {
+         alert(`The score of ${score} was added to the the movie "${name}"`);
+         this.setState(() => {
+          return ({
+            hideNewMovieForm : true,
+            hideRankMovieForm : true,
+            hideSearchMovieForm : true,
+            hideAddNewMovieButton : false,
+            hideRankMovieButton : false,
+            hideSearchMovieButton : false,
+            hideErrorMessage : true,
+            list:[]
+          });
+         });
+       }).catch((error) => {
+         console.log(error);
+       })
+    
+      }     
+    })
+     .catch((error) => {
+      console.log(error);
+    });    
+  };
+  handleSearchMovie(){
+    this.setState(() => {
+      return ({
+        hideNewMovieForm : true,
+        hideRankMovieForm : true,
+        hideSearchMovieForm : false,
+        hideAddNewMovieButton : false,
+        hideRankMovieButton : false,
+        hideSearchMovieButton : true,
+        hideErrorMessage : true,
+        list:[]
+      });
+    });   
+  };
+  handleSearchMovieSubmit(e){
+    e.preventDefault();
+    const fromYear = document.getElementById("fromYear").value;
+    const toYear = document.getElementById("toYear").value;
+    const searchGenre = document.getElementById("searchMovieGenre").value.toLowerCase();
+    console.log(fromYear, toYear, searchGenre);
+    if ((!searchGenre) || (!fromYear) || (!toYear)){
+      this.setState(() => {
+        return ({
+          hideErrorMessage :false
+        });
+      });
+      return console.log("error");
+    }
+    this.setState(() => {
+      return ({
+        hideErrorMessage : true
+      });
+    });
+    axios.get(`../movies/search?genre=${searchGenre}&from=${fromYear}&to=${toYear}`)
+    .then((response) => {
+      if (response.data.movies.length === 0){
+        this.setState(() => {
+          return ({
+            list: <p>No movies were found</p>
+          });
+        });
+      } else{
+        let movieData = response.data.movies.map((movie) =>{
+          let sum = 0;
+          for (var i=0; i<movie.score.length; i++){
+            sum += parseInt(movie.score[i] ,10);
+          }
+          let avarage = sum/movie.score.length;
+        return (<li>Name: {movie.movie_name} Avarage score: {avarage || 'No ratings'} </li>);
+        });
+        this.setState(() =>{
+          return ({
+            list: movieData
+          });
+        });
+      }
+      console.log(response.data.movies);
+    }).catch((error) => {
+      console.log(error);
+    });
+
+  };
+  // handleButton(){
+  //   axios.get('../movies').then((res) => {
+  //     console.log(res);
+  //   }).catch((e) => {
+  //     console.log(e);
+  //   })
+  // };  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-         HELLLLLLOOOOO!!!!
-        </p>
-        <button onClick={this.handleButton}>Get something from server</button>
+        <h1 className="App-title">Welcome to My Movie App</h1>        
+        <button 
+          className='button' 
+          onClick={this.handleAddNewMovie}
+          hidden={this.state.hideAddNewMovieButton}
+          >Add new movie</button>
+          <br/>
+        <form hidden={this.state.hideNewMovieForm}>
+          <fieldset>
+          <h3>Add Movie</h3>
+          <p 
+          hidden={this.state.hideErrorMessage}
+          className="ErrorMessage">
+          All data fields are required</p>
+          Name: 
+          <input type="text" placeholder="Movie name" id="newMovieName"/><br/>
+          Genre: 
+          <select id="newMovieGenre"> 
+            <option value="" default selected>Select Genre</option>
+            <option value="Comedy">Comedy</option>           
+            <option value="Drama">Drama</option>
+            <option value="Sci-Fi">Sci-Fi</option>
+            <option value="Thriller">Thriller</option>
+          </select><br/>
+          Release Year: 
+          <input 
+            placeholder="Year" 
+            type="number" 
+            min="1888" 
+            max="2018"
+            id="newMovieYear"
+            /><br/><br/>
+          <input type="submit" value="Submit movie" onClick={this.handleSubmitNewMovie}/>
+          </fieldset>
+        </form><br/>
+        <br/>        
+        <button 
+          className='button' 
+          onClick={this.handleRateMovie}
+          hidden={this.state.hideRankMovieButton}
+          >Rate a movie</button>
+          <br/>
+        <form hidden={this.state.hideRankMovieForm}> 
+        <fieldset>
+        <h3>Rate Movie</h3>
+        <p 
+          hidden={this.state.hideErrorMessage}
+          className="ErrorMessage">
+          All data fields are required</p>
+          Name: 
+          <input type="text" placeholder="Movie name" id="rankMovieName"/><br/>
+          Score (1-10): 
+          <input placeholder="Score" type="number" min="1" max="10" id="score"/><br/>
+          <input type="submit" value="Submit score" onClick={this.handleSubmitScore}/>
+          </fieldset>
+        </form> <br/>
+        <br/>
+        <button 
+          className='button' 
+          onClick={this.handleSearchMovie}
+          hidden={this.state.hideSearchMovieButton}
+          >Search movie</button><br/>
+        <form hidden={this.state.hideSearchMovieForm}>
+        <fieldset>
+          <h3>Search Movie</h3>
+          <p 
+          hidden={this.state.hideErrorMessage}
+          className="ErrorMessage">
+          All data fields are required</p>
+          Release year:   
+        <input placeholder="From" type="number" min="1888" max="2018" id="fromYear"/>
+        <input placeholder="To" type="number" min="1888" max="2018" id="toYear"/><br/>
+          Genre: 
+        <select id="searchMovieGenre"> 
+            <option value="" default selected>Select Genre</option>
+            <option value="Comedy">Comedy</option>           
+            <option value="Drama">Drama</option>
+            <option value="Sci-Fi">Sci-Fi</option>
+            <option value="Thriller">Thriller</option>          
+          </select><br/>
+          <input type="submit" value="Search" onClick={this.handleSearchMovieSubmit}/>
+          <ul> 
+            {this.state.list}
+          </ul>
+          </fieldset>
+          </form>
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = process.env.PORT || 5000
 
 
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+app.use(bodyParser.json());
+
 const mySqlHost = "localhost";
 
-
-
-app.get('/', (req, res) => {
-    res.set('Content-Type', 'application/json');
-    res.send('Hello from node server');
-});
-app.get('/movies/', (req, res) => {
-    console.log('get request received')
+app.post('/movies', (req, res) => {   
+    let movie_name = req.body.movie_name;
+    let genre = req.body.genre;
+    let release_year= req.body.release_year;
+    //add new movie to DB
     const connection = mysql.createConnection({
         host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         user:'vddz3a51wdk49rc4',
@@ -26,15 +27,90 @@ app.get('/movies/', (req, res) => {
             return console.log(error);
         }    
         console.log('Connected!');
-    })
-    connection.query("SELECT * FROM x00qcbxug94h1hki.movies", (error, result) => {
+    });
+    connection.query(`INSERT INTO x00qcbxug94h1hki.movies (movie_name, genre, release_year) VALUES ('${movie_name}', '${genre}', '${release_year}')`, (error, result) => {
+        if (error){
+            res.status(400).send(error);
+        }
+        res.send(result);
+    });
+    connection.end();
+});
+app.get('/movies', (req,res) => {
+    let movie = req.query.movie_name;  
+    const connection = mysql.createConnection({
+        host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user:'vddz3a51wdk49rc4',
+        password:'pxh153k7dxyuvt4n'
+    });
+    connection.connect((error) => {
         if (error){
             return console.log(error);
-        }  
+        }    
+        console.log('Connected!');
+    });
+    
+    connection.query(`SELECT * FROM x00qcbxug94h1hki.movies where movie_name = "${movie}"`, (error, result) => {
+        if (error){
+            res.status(400).send(error);
+        }
         res.send(result);
-    });  
-    connection.end();  
-})
+
+    });
+    connection.end();
+      
+});
+app.get('/movies/search', (req, res) => {
+    let genreToMatch = req.query.genre;
+    let fromYear = req.query.from -1;
+    let toYear = req.query.to +1;
+    //return movies by year and genre
+    res.send('return movies by year and genre');
+});
+app.patch('/movies/', (req,res) => {
+    let movie = req.query.movie_name; 
+    let newScore = parseInt(req.query.score, 10);
+    const connection = mysql.createConnection({
+        host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user:'vddz3a51wdk49rc4',
+        password:'pxh153k7dxyuvt4n'
+    });
+    connection.connect((error) => {
+        if (error){
+            return console.log(error);
+        }    
+        console.log('Connected!');
+    });
+    let currentAvg, numOfRatings;
+    connection.query(`SELECT avarage_rating, number_of_ratings FROM x00qcbxug94h1hki.movies where movie_name = "${movie}"`,(error, result) => {
+        if (error){
+            res.status(400).send(error);
+        }        
+        currentAvg = parseFloat(result[0].avarage_rating, 10);
+        numOfRatings = parseInt(result[0].number_of_ratings, 10); 
+        numOfRatings++;
+        let newAvarage = ((currentAvg * (numOfRatings-1)) + newScore) / numOfRatings;
+        console.log('currentAvg', currentAvg);
+        console.log('newScore: ', newScore);
+        console.log('numOfRatings: ', numOfRatings );
+        console.log('newAvarge: ', newAvarage);            
+        let sql = `UPDATE x00qcbxug94h1hki.movies SET avarage_rating = ${newAvarage}, number_of_ratings = ${numOfRatings} WHERE movie_name= "${movie}"`
+        connection.query(sql,
+            (error, result) => {
+            if (error){
+                console.log(error);
+                res.status(400).send(error);
+            }
+            res.send(result);
+        });
+        connection.end();
+    });
+    
+});
+app.get('/', (req, res) => {
+    res.set('Content-Type', 'application/json');
+    res.send('Hello from node server');
+});
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
